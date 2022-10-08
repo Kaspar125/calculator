bd15a3191a77fc69151c0a5584fa180175e6086b
Kaspar <kaasik.kaspar22@gmail.com>
2022-10-08 18:37:32 +0300

    prototype ready

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 function initialize() {
     let allButtons = document.getElementsByTagName("button");
     for (let i = 0; i < allButtons.length; i++) {
-        allButtons[i].addEventListener("click", function(btn) {
+        allButtons[i].addEventListener("click", function (btn) {
             addNumber(btn.target.innerText);
         })
     }
 }
 
 function addNumber(value) {
+    let userInput = document.getElementById('input');
+    let oldValue = userInput.value;
+    if (value === 'clear') {
+        userInput.value = '';
+    } else if (value === '=') {
+        let calculateValue = eval(oldValue);
+        let result = document.getElementById('result');
+        userInput.value = calculateValue;
+        result.innerText = oldValue + '  = ' + calculateValue;
+    } else if (['+', '-', '/', '*'].includes(value)) {
+        userInput.value = oldValue + ' ' + value + ' ';
+    } else {
+        userInput.value = oldValue + value;
+    }
     console.log(value);
 }
 
-window.addEventListener("load",function() {
+window.addEventListener("load", function () {
     initialize();
 });
